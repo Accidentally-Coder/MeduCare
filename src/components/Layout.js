5fbf86d48db0bcecadd26b9c314793c751f09479
mainul3195 <mainul3195@gmail.com>
2023-10-15 04:26:34 +0600

    added new cards

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -20,13 +20,15 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Sidebar from './Sidebar';
+import Hero from './Hero';
 
 const Layout = (props) => {
   return (
     <div>
       <Header />
       <main style={{ minHeight: '100vh', display: 'flex' }}>
-        <Sidebar />
+        {/* <Sidebar/> */}
+        <Hero />
         <div style={{ flex: 1 }}>
           {props.children}
         </div>

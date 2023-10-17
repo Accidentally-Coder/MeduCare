820002014cb78ac81c2e709e30681ab68f0199ef
Farzana Reefat Raha <reefat.raha2018@gmail.com>
2023-10-17 15:53:38 +0600

    Completed Blog Backend

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Blogs = require("../models/blogModel");
-const { saveBlogController } = require('../controllers/blogController');
+const { saveBlogController, getAllBlogsController, increaseLikesController, increaseDislikesController, decreaseLikesController, decreaseDislikesController } = require('../controllers/blogController');
 
 // router object
 const router = express.Router();
@@ -9,5 +9,14 @@ const router = express.Router();
 
 // SAVE BLOG || METHOD POST
 router.post('/save-blog', saveBlogController);
-
+// GET ALL BLOG || METHOD GET
+router.get('/get-all-blogs', getAllBlogsController);
+// INCREASE LIKES || METHOD PUT
+router.put('/increase-likes/:blogId', increaseLikesController);
+// INCREASE DISLIKES || METHOD PUT
+router.put('/increase-dislikes/:blogId', increaseDislikesController);
+// DECREASE LIKES || METHOD PUT
+router.put('/decrease-likes/:blogId', decreaseLikesController);
+// DECREASE DISLIKES || METHOD PUT
+router.put('/decrease-dislikes/:blogId', decreaseDislikesController);
 module.exports = router;
